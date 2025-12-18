0e9adbe4f573a441f47a7e0718ad06f70bde67ab
Empire Tech <151479661+efeurhobobullish@users.noreply.github.com>
2025-12-18 15:04:18 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { Toaster } from "sonner";
 import { ScrollToTop } from "@/components/ui";
 import { Home, Splash } from "@/pages";
+import { Login } from "@/pages/auth";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Splash />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
